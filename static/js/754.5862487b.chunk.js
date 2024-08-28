aeb96f845ca28135ed115e25fb362a42783909ec
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-08-28 15:03:04 +0000

    Deploying to gh-pages from @ KMIS2008/FinancialScorel@eebb22e17cacb9f054012c1cb3ec522dcd11d9d2 🚀

---
+++ static/js/754.5862487b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkonline_shop_pharmacy=self.webpackChunkonline_shop_pharmacy||[]).push([[754],{2754:function(n,e,r){r.r(e),r.d(e,{default:function(){return a}});var u=r(184);function a(){return(0,u.jsx)(u.Fragment,{})}}}]);
+//# sourceMappingURL=754.5862487b.chunk.js.map
